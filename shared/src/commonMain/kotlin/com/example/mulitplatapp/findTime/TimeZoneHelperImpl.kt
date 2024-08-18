8049387e6bf671d4c74781d35ef0f502333f8007
narges.dipir <nargesdipir@gmail.com>
2024-08-18 10:32:20 +0200

    time zone interface and implementation

---
+++ shared/src/commonMain/kotlin/com/example/mulitplatapp/findTime/TimeZoneHelperImpl.kt
@@ -0,0 +1,32 @@
+package com.example.mulitplatapp.findTime
+
+class TimeZoneHelperImpl: TimeZoneHelper {
+
+    override fun getTimeZoneStrings(): List<String> {
+        TODO("Not yet implemented")
+    }
+
+    override fun currentTime(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun currentTimeZone(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun hoursFromTimeZone(otherTimeZoneId: String): Double {
+        TODO("Not yet implemented")
+    }
+
+    override fun getTime(timeZoneId: String): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun getDate(timeZoneId: String): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun search(startHour: Int, endHour: Int, timeZoneString: List<String>): List<Int> {
+        TODO("Not yet implemented")
+    }
+}
