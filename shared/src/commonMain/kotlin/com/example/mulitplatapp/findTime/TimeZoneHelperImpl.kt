c3806432d6b5935452247eefd3646f1d9dc629a9
narges.dipir <nargesdipir@gmail.com>
2024-08-18 10:47:00 +0200

    time zone implementation : formatDateTime

--- shared/src/commonMain/kotlin/com/example/mulitplatapp/findTime/TimeZoneHelperImpl.kt
+++ shared/src/commonMain/kotlin/com/example/mulitplatapp/findTime/TimeZoneHelperImpl.kt
@@ -1,5 +1,6 @@
 package com.example.mulitplatapp.findTime
 
+import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 
 class TimeZoneHelperImpl: TimeZoneHelper {
@@ -30,5 +31,21 @@ class TimeZoneHelperImpl: TimeZoneHelper {
 
     override fun search(startHour: Int, endHour: Int, timeZoneString: List<String>): List<Int> {
         TODO("Not yet implemented")
+    }
+
+    fun formatDateTime(dateTime: LocalDateTime): String {
+        val stringBuilder = StringBuilder()
+        val minute = dateTime.minute
+        var hour = dateTime.hour % 12
+        if (hour == 0) hour = 12
+        val amPm = if (dateTime.hour < 12) " am" else " pm"
+        stringBuilder.append(hour.toString())
+        stringBuilder.append(":")
+        if (minute < 10) {
+            stringBuilder.append('0')
+        }
+        stringBuilder.append(minute.toString())
+        stringBuilder.append(amPm)
+        return stringBuilder.toString()
     }
 }
