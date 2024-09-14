ef1597066c1a5b1928f24f67f0636121ae93451a
narges.dipir <nargesdipir@gmail.com>
2024-09-14 16:27:11 +0200

    android side : ui package : MainView : BottomItem class

--- multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/MainView.kt
+++ multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/MainView.kt
@@ -1,7 +1,15 @@
 package com.example.mulitplatapp.android.ui
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 
 sealed class Screen(val title: String) {
     object TimeZonesScreen : Screen("TimeZones")
     object AddTimeZoneScreen : Screen("Find Time")
 }
+
+data class BottomItem(
+    val route: String,
+    val icon: ImageVector,
+    val iconContentDescription: String
+)
