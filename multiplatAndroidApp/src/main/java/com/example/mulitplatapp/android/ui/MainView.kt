3118e8aaa96b962137ce6bde13dc1d3c998cc766
narges.dipir <nargesdipir@gmail.com>
2024-09-14 17:00:31 +0200

    android side : commit demo

--- multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/MainView.kt
+++ multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/MainView.kt
@@ -20,4 +20,5 @@ data class BottomItem(
 val bottomNavigationItems = listOf(
     BottomItem(Screen.TimeZonesScreen.title, Icons.Rounded.Info, "Timezones"),
     BottomItem(Screen.AddTimeZoneScreen.title, Icons.Rounded.Place, "Find Time")
+
 )
