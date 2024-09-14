2f0b1b432012c4bf96a0aeea21cb1b89399e9ad7
narges.dipir <nargesdipir@gmail.com>
2024-09-14 16:34:44 +0200

    android side : ui package : MainView : Bottom navigation items

--- multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/MainView.kt
+++ multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/MainView.kt
@@ -1,5 +1,9 @@
 package com.example.mulitplatapp.android.ui
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Place
 import androidx.compose.ui.graphics.vector.ImageVector
 
 
@@ -12,4 +16,9 @@ data class BottomItem(
     val route: String,
     val icon: ImageVector,
     val iconContentDescription: String
+)
+
+val bottomNavigationItems = listOf(
+    BottomItem(Screen.TimeZonesScreen.title, Icons.Rounded.Info, "Timezones"),
+    BottomItem(Screen.AddTimeZoneScreen.title, Icons.Rounded.Place, "Find Time")
 )
