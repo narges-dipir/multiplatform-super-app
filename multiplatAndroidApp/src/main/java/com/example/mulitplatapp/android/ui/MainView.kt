c419683e8e41ef19b40608432d230bedef586c0c
narges.dipir <nargesdipir@gmail.com>
2024-09-14 17:22:03 +0200

    android side : MapView : creation

--- multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/MainView.kt
+++ multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/MainView.kt
@@ -3,12 +3,18 @@ package com.example.mulitplatapp.android.ui
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material.icons.rounded.Place
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.Snapshot
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.mulitplatapp.android.MyApplicationTheme
 
 
 sealed class Screen(val title: String) {
-    object TimeZonesScreen : Screen("TimeZones")
-    object AddTimeZoneScreen : Screen("Find Time")
+    data object TimeZonesScreen : Screen("TimeZones")
+    data object AddTimeZoneScreen : Screen("Find Time")
 }
 
 data class BottomItem(
@@ -20,5 +26,23 @@ data class BottomItem(
 val bottomNavigationItems = listOf(
     BottomItem(Screen.TimeZonesScreen.title, Icons.Rounded.Info, "Timezones"),
     BottomItem(Screen.AddTimeZoneScreen.title, Icons.Rounded.Place, "Find Time")
-
 )
+
+@Composable
+fun MapView(
+    actionBarFun: topBarFun = { EmptyComposable() }
+) {
+    val showAddDialog = remember {
+        mutableStateOf(false)
+    }
+    val currentTimezoneStrings = remember {
+        SnapshotStateList<String>()
+    }
+    val selectedIndex = remember {
+        mutableStateOf(0)
+    }
+
+    MyApplicationTheme {
+
+    }
+}
