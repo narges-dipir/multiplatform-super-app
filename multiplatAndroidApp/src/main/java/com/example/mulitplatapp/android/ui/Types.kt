67580df5f09681d0313d22a0c5e5255161c978bc
narges.dipir <nargesdipir@gmail.com>
2024-09-14 16:22:25 +0200

    android side : ui package : Types

---
+++ multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/Types.kt
@@ -0,0 +1,12 @@
+package com.example.mulitplatapp.android.ui
+
+import androidx.compose.runtime.Composable
+
+typealias OnAddType = (List<String>) -> Unit
+typealias OnDismissType = () -> Unit
+typealias composeFun = @Composable () -> Unit
+typealias topBarFun = @Composable (Int) -> Unit
+
+@Composable
+fun EmptyComposable() {
+}
