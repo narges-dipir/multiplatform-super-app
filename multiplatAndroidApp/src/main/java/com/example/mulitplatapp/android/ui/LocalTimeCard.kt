0daf8d1eeafddb2daf9a14f30b9b7e9dfd5460dc
narges.dipir <nargesdipir@gmail.com>
2024-09-15 16:15:25 +0200

    android side : LocalTime Card : add two identical columns

--- multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/LocalTimeCard.kt
+++ multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/LocalTimeCard.kt
@@ -3,16 +3,24 @@ package com.example.mulitplatapp.android.ui
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.mulitplatapp.android.endGradientColor
+import com.example.mulitplatapp.android.startGradientColor
 
 
 @Composable
@@ -29,7 +37,40 @@ fun LocalTimeCard(city: String, time: String, date: String) {
             border = BorderStroke(1.dp, Color.Black),
             modifier = Modifier.fillMaxWidth()
         ) {
-
+            Box(
+                modifier = Modifier
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                startGradientColor,
+                                endGradientColor
+                            )
+                        )
+                    )
+                    .padding(8.dp)
+            )
+            {
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    Column(
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        Text(text = "Your Location", style = MaterialTheme.typography.bodySmall)
+                        Spacer(modifier =Modifier.height(8.dp))
+                        Text(text = city, style = MaterialTheme.typography.headlineSmall)
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                    Column(
+                        horizontalAlignment = Alignment.End
+                    ) {
+                        Spacer(modifier = Modifier.weight(1.0f))
+                        Text(text = "Time", style = MaterialTheme.typography.headlineSmall)
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(text = time, style = MaterialTheme.typography.bodySmall)
+                        Spacer(modifier = Modifier.height(8.dp))
+                    }
+                }
+            }
         }
     }
 }
