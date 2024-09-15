f40e77ac5ed9a1725d3853eae9aad7010d30cdfd
narges.dipir <nargesdipir@gmail.com>
2024-09-15 16:45:28 +0200

    android side : TimeZoneScreen : add the main column

--- multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/TimeZoneScreen.kt
+++ multiplatAndroidApp/src/main/java/com/example/mulitplatapp/android/ui/TimeZoneScreen.kt
@@ -1,13 +1,22 @@
 package com.example.mulitplatapp.android.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.mulitplatapp.findTime.TimeZoneHelper
 import com.example.mulitplatapp.findTime.TimeZoneHelperImpl
+import kotlinx.coroutines.delay
 
 
 const val timeMillis = 1000 * 60L
@@ -22,6 +31,20 @@ fun TimeZoneScreen(
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-
+        var time by remember {
+            mutableStateOf(timezoneHelper.currentTime())
+        }
+        LaunchedEffect(Unit) {
+            while (true) {
+                time = timezoneHelper.currentTime()
+                delay(timeMillis)
+            }
+        }
+        LocalTimeCard(
+            city = timezoneHelper.currentTime(),
+            time = time,
+            date = timezoneHelper.getDate(timezoneHelper.currentTimeZone())
+        )
+        Spacer(modifier = Modifier.size(16.dp))
     }
 }
